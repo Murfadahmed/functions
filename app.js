9d4cda672bc7005d091748d84915e8b61231d2ed
murfadahmed <malikmurfad361@gmail.com>
2023-02-21 22:32:40 +0500

    incomplete yet

---
+++ app.js
@@ -0,0 +1,58 @@
+
+//-----------FUNTIONS---------------
+//-------------question number 01----------
+
+
+// function liveDate (){
+//     var date = new Date()
+//     var dateString = date.toLocaleString();
+//     alert(`the current date & time in pak is ${dateString}`)
+//     console.log(`the current date & time in pak is ${dateString}`)
+//     document.write (`the current date & time in pak is ${dateString}`)
+// }
+// liveDate();
+
+//------------question number 02----
+
+
+// function greeting(){
+//     var first = prompt("write your first name")
+//     var last = prompt("write your last name")
+//     alert(`Assalam o alikum ${first}${last}!`)
+// }
+// greeting()
+
+// var num3 = prompt("write a OPERATER ")
+//------------question number 03----------
+
+
+// function math(num1,num2){
+// return (num1 + num2)
+// }
+// var num1 = +prompt("write a first number")
+// var num2 = +prompt("write a second number")
+
+// document.write(math(num1,num2))
+
+//----------QUESTION NUMBER 04-------
+
+// var num1 = +prompt("write a first number")
+// var num3 = prompt("write a OPERATER ")
+// var num2 = +prompt("write a second number")
+// function desire(num1,num2,num3){
+//     return (`${num1} ${num3} ${num2}`)
+// }
+// alert(desire(num1,num2))
+//-------------question  number 05-------
+
+
+// var typeOfSquare = +prompt("write a number o you want to take double")
+
+// function square (typeOfSquare){
+
+//     return typeOfSquare **2
+// }
+// document .write(square(typeOfSquare))
+
+
+0000000000000000// queation Number  06 
